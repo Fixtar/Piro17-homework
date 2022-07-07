1afa0363f610c55e8e8fece0b252127422536b82
seongha <11pi885@gmail.com>
2022-07-08 01:52:51 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -5,31 +5,36 @@ num = 0;
 
 #3단계
 while 1:
-    try:
-        x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'));
-    except:
-        print('정수를 입력하세요');
-    else:
-        if x < 1 or x > 3:
-          print('1,2,3 중 하나를 입력하세요');
+    while 1:
+        try:
+            x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'));
+        except:
+            print('정수를 입력하세요');
         else:
-            break;
-#4단계
-for i in range(x):
-    num+=1;
-    print('playerA :', num);
+            if x < 1 or x > 3:
+                print('1,2,3 중 하나를 입력하세요');
+            else:
+                break;
+    #4단계
+    for i in range(x):
+        num+=1;
+        print('playerA :', num);
+        if num >=31:
+            exit();
 
-#5단계
-while 1:
-    try:
-        x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'));
-    except:
-        print('정수를 입력하세요');
-    else:
-        if x < 1 or x > 3:
-          print('1,2,3 중 하나를 입력하세요');
+    #5단계
+    while 1:
+        try:
+            x = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'));
+        except:
+            print('정수를 입력하세요');
         else:
-            break;
-for i in range(x):
-    num+=1;
-    print('playerB :', num);
+            if x < 1 or x > 3:
+                print('1,2,3 중 하나를 입력하세요');
+            else:
+                break;
+    for i in range(x):
+        num+=1;
+        print('playerB :', num);
+        if num >=31:
+            exit();
